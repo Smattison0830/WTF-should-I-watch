a17042e7a1043f440cd997530c98e721d7602064
Spencer Mattison <spenceramattison@gmail.com>
2021-07-13 21:46:03 +0900

    added image upload to list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -25,7 +25,7 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :id, :list_id)
+    params.require(:list).permit(:name, :id, :list_id, :photo)
   end
 
 end
